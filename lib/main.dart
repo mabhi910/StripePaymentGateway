f2d9aeba7a2d69b9750d0777b1672adf5b121441
mabhi910 <102281416+mabhi910@users.noreply.github.com>
2024-02-02 11:01:31 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   //Assign publishable key to flutter_stripe
-  Stripe.publishableKey = "pk_test_51KhRV7SAWPAAIeuzLCz7vHZ2ytUfzxJsXZzqcWgJHaylAKZdo9rqZaucs50bvbNn5ftUMe6K39lN1ixvLkWXYYGt00AL2KCtJg";
+  Stripe.publishableKey = "test key";
 
   //Load our .env file that contains our Stripe Secret key
   await dotenv.load(fileName: "assets/.env");
